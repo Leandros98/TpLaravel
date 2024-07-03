2bc162616f9d016f240de871e579202d84a450a8
Leandre NIMPAGARITSE <leandrenimpagaritse22@gmail.com>
2024-07-03 19:53:00 +0200

    Mise en place des fonctionalites de l'Admin

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::resource('property',\App\Http\Controllers\Admin\PropertyController::class)->except(['show']);
+    Route::resource('option',\App\Http\Controllers\Admin\OptionController::class)->except(['show']);
+});
 
 require __DIR__.'/auth.php';
